a5ceda091f3dc7626b6f7e5bd8cdc1e4767354e4
Lasse <laje0005@stud.kea.dk>
2025-01-08 14:36:38 +0100

    Noter til JS

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,14 +1,14 @@
 const burger = document.querySelector(".burger"); /* Stregerne */ 
 const nav = document.querySelector("nav");
-const menu = document.querySelector(".menu");
-const links = document.querySelectorAll(".menu li a");
+const menu = document.querySelector(".menu"); /* Dropdown */
+const links = document.querySelectorAll(".menu li a"); /* Links i dropdown */
 
 burger.addEventListener("click", () => {
   burger.classList.toggle("active");
   nav.classList.toggle("active");
 });
 
-links.forEach((link) => {
+links.forEach((link) => { /* Fjerner "active", når der trykkes på et link */
   link.addEventListener("click", () => {
     burger.classList.remove("active");
     nav.classList.remove("active");
